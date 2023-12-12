258989b1d5322aa36a59c50ecce9c68a382eac54
MarcelJiang <90645034+MarcelJiang@users.noreply.github.com>
2023-12-12 11:00:26 +0100

    Frontend aufgesetzt

--- CodechatterMongo2/Spg.Codechatter.Infrastructure/obj/Debug/net7.0/Spg.Codechatter.Infrastructure.AssemblyInfo.cs
+++ CodechatterMongo2/Spg.Codechatter.Infrastructure/obj/Debug/net7.0/Spg.Codechatter.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spg.Codechatter.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b76a3f53e8c0175dcc19afe46af72d9da88b03be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b74312ddcf4e67f76166bb157dd8df49f18d7f9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spg.Codechatter.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spg.Codechatter.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import useSWR from 'swr'
+
+export default function Home() {
+  return (
+    
+   <h1>hello</h1>
+  )
+}
